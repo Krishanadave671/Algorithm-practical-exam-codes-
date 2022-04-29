1f90db262f54aca5125c8d60607d46df4fd45271
krishana dave <krishnadave671@gmail.com>
2022-04-29 08:05:31 +0530

    insertion, selection seperated

--- 01_selectionsort.cpp
+++ 01_selectionsort.cpp
@@ -1,17 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void bubbleSort(int *arr, int n){
-    int i, j ;
-    for(i = 0; i < n - 1; i++){
-        for(j = 0; j < n - i - 1 ; j++){
-            if(arr[j] > arr[j + 1]){
-                swap(arr[j], arr[j + 1]);
-            }
-
-        }
-    }
-}
 void selectionSort(int *arr, int n){
      int i, j;
     for (i = 0; i < n-1; i++){
@@ -24,18 +13,7 @@ void selectionSort(int *arr, int n){
         swap(arr[minidx], arr[i]);
     }
 }
-void insertionSort(int *arr  , int n){
-    int i , key , j ;
-    for(i = 1 ; i < n   ; i ++){
-        key = arr[i];
-        j = i - 1 ;
-        while( j >= 0  and arr[j] > key ){
-            arr[j + 1] = arr[j];
-            j --;
-        }
-        arr[j + 1] = key ;
-    }  
-}
+
 void printarray(int *arr , int n){
      
      for(int i = 0 ; i < n ; i++){
--- 03_Mergesort.cpp
+++ 03_Mergesort.cpp
@@ -1,19 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std ;
 
-
-int partition(int *arr, int l , int r){
-    int pivot = arr[r];
-    int i = l - 1;    
-    for(int j = l; j < r ; j++){
-        if(arr[j] < pivot){   
-            i++;
-            swap(arr[i], arr[j]);
-        }
-    }
-    swap(arr[i + 1], arr[r]);
-    return i + 1;
-}
 void merge(int *arr, int l ,int mid, int r){
     int n1 = mid - l + 1;
     int n2 = r - mid ;
@@ -58,13 +45,7 @@ void printarray(int *arr , int n){
      }
     
 }
-void quicksort(int *arr, int l , int r){
-    if(l < r){
-        int pi = partition(arr, l, r);
-        quicksort(arr , l , pi - 1);
-        quicksort(arr,pi + 1, r);
-    }
-}
+
 void mergesort(int *arr, int l , int r){
     if(l < r){
         int mid = (l +  r)/2 ;
